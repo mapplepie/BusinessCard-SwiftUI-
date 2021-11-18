8eee134f6b560ef55c739b20841be4557c688257
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-18 18:39:14 +0900

    add custom font

--- BusinessCard(SwiftUIPractice)/ContentView.swift
+++ BusinessCard(SwiftUIPractice)/ContentView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        ZStack{
+                    Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Mapplepie")
+                            .font(Font.custom("Merkur", size: 40))
+                            .bold()
+                            .foregroundColor(.white)
+                        .padding()
+                Text("iOS Developer")
+                
+                    .foregroundColor(.white)
+
+            }
+                              
+            }
+        }
+ }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

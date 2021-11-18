4995a265b512391f5f466ea89f123d6b896fdc36
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-18 16:29:29 +0900

    Initial Commit

---
+++ BusinessCard(SwiftUIPractice)/BusinessCard_SwiftUIPractice_App.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCard_SwiftUIPractice_App.swift
+//  BusinessCard(SwiftUIPractice)
+//
+//  Created by Mai Uchida on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCard_SwiftUIPractice_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
